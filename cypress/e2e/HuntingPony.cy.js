a8ade939963b09fb1700ca968665b25d99c215f1
Denis <132175078+DenisTolstikov-qa@users.noreply.github.com>
2023-05-27 00:31:08 +0300

    Update HuntingPony.cy.js

--- cypress/e2e/HuntingPony.cy.js
+++ cypress/e2e/HuntingPony.cy.js
@@ -5,10 +5,12 @@ describe('Оформление заказа', function () {
         cy.get('[data-index="1"] > .header__collections-controls > .header__collections-link').click();
         cy.get('.without-sku.is-available > .product-preview__content > .product-preview__area-bottom').trigger('mouseover');
         cy.get('.without-sku.is-available > .product-preview__content > .product-preview__area-title > .product-preview__title > a').click();
+        cy.wait(5000)
         cy.get('.add-cart-counter__btn').click();
+        cy.wait(5000)
         cy.get('.header__cart > .icon').click();
-        cy.get('[data-add-cart-counter-plus=""]').click();
-        cy.get('.is-count-up').click();
+        cy.wait(5000)
+        cy.get('.is-count-up > .icon').click();
         cy.get('.cart-controls > .button').click();
         cy.contains('Оформление заказа')
         })
